94a1ea6828f9aabe43351cce9046a17db955a1e4
Sol <ykim327@my.bcit.ca>
2025-03-01 22:55:50 -0800

    Switch Bootstrap from CDN to LibMan

--- Lab5/Models/Post.cs
+++ Lab5/Models/Post.cs
@@ -1,13 +1,28 @@
 ﻿namespace Lab5.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;  
+
     public class Post
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public DateTime DatePosted { get; set; }
 
-         public List<Comment> Comments { get; set; } = new List<Comment>();
+        [Required(ErrorMessage = "User name is required.")]
+        [StringLength(50, ErrorMessage = "User name must be less than 50 characters.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(100, ErrorMessage = "Title must be less than 100 characters.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Content is required.")]
+        [StringLength(1000, ErrorMessage = "Content must be less than 1000 characters.")]
+        public string Content { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DatePosted { get; set; } = DateTime.Now; 
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
     }
 }
