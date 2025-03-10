e61fd94f8af98993c8d6287476d9e24c9d3d5c13
Sol <ykim327@my.bcit.ca>
2025-03-10 15:57:05 -0700

    add appsetting.json

---
+++ Lab5/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab5.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                    new User
+                    {
+                        Id = 1,
+                        Name = "Mary",
+                        Email = "mary@pragimtech.com"
+                    },
+                    new User
+                    {
+                        Id = 2,
+                        Name = "John",                       
+                        Email = "john@pragimtech.com"
+                    }
+                );
+        }
+    }
+}
---
+++ Lab5/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab5.Models
+{
+    public class User : IdentityUser
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(20)]
+        public string Name { get; set; }
+
+        [Display(Name = "Office Email")]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+            ErrorMessage = "Invalid email format")]
+        [Required]
+        public string Email { get; set; }
+    }
+}
